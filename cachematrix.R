b594a626e0a9c0504b2f2be4e2aa2346f8c5301d
Radu-Nicoara <Radu.Nicoara@isdc.eu>
2015-05-18 13:33:17 +0300

    minor adjustments in comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,11 +2,11 @@
 ## the inverse of a matrix rather than compute it repeatedly 
 
 
-## makeCacheMatrix creates a placeholder for the cahed value. 
+## makeCacheMatrix creates a placeholder for the cached value. 
 ## It contains 2 holders for the original matrix and for it's cached inverse (mx and solve)
 ## and also has 4 functions: a 2 set/get pairs for each  holder. The set of the matrix object is also resetting 
 ## the cached value of the inverse
-## publicly is exposing just the 2 set/get pairs: set,get,setsolve,getsolve
+## publicly is exposing just a vector with 4 functions: the 2 set/get pairs: set, get, setsolve, getsolve
 
 makeCacheMatrix <- function(mx = matrix()) {
     solve <- NULL
@@ -31,8 +31,8 @@ makeCacheMatrix <- function(mx = matrix()) {
 
 
 ## cacheSolve returns a matrix that is the inverse of x
-## it the inverse of x was not calculated before, it will be calculated and then cached
-## it is was calculated and cached, it will be returned without need for recalculation
+## if the inverse of x was not calculated before, it will be calculated and then cached
+## if it was calculated and cached, it will be returned without need for recalculation
 
 cacheSolve <- function(x, ...) {
     ## try to get the cached inverse
